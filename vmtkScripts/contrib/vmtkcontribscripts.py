347085ea4e5f93119a1803df812a4a56bd0ebee8
Marco Fedele <fedele.marco@gmail.com>
2019-07-04 22:35:40 +0200

    script to solve heat equation on a surface and (optionally) divide it in harmonic section (author Elena Faggiano)

--- vmtkScripts/contrib/vmtkcontribscripts.py
+++ vmtkScripts/contrib/vmtkcontribscripts.py
@@ -22,6 +22,7 @@ __all__ = [
     'vmtk.vmtknumpytosurface',
     'vmtk.vmtknumpywriter',
     'vmtk.vmtksurfaceconnector',
+    'vmtk.vmtksurfaceharmonicsections',
     'vmtk.vmtksurfaceextractinnercylinder',
     'vmtk.vmtksurfaceresolution',
     'vmtk.vmtksurfacetagger',
