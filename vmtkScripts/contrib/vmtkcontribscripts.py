5366a34ae6999858b6f67e3486297e4df064a72b
Marco Fedele <marco.fedele@polimi.it>
2020-05-21 12:01:24 +0200

    vtkFeatureEdges as a script

--- vmtkScripts/contrib/vmtkcontribscripts.py
+++ vmtkScripts/contrib/vmtkcontribscripts.py
@@ -24,6 +24,7 @@ __all__ = [
     'vmtk.vmtknumpytosurface',
     'vmtk.vmtknumpywriter',
     'vmtk.vmtksurfaceconnector',
+    'vmtk.vmtksurfacefeatureedges',
     'vmtk.vmtksurfaceharmonicconnector',
     'vmtk.vmtksurfaceharmonicextension',
     'vmtk.vmtksurfaceharmonicsections',
